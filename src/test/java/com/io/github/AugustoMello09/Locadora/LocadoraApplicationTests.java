00b273b945d8f1cb9352e31b0ac7f01d6471d0b0
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-04-09 10:28:17 -0300

    projeto criado

---
+++ src/test/java/com/io/github/AugustoMello09/Locadora/LocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.github.AugustoMello09.Locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
